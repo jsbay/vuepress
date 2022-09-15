9e0d978f86ec74cdf8953dbbea74fb4ac2cb41b0
jsbay <13140111181@163.com>
2022-09-15 02:05:11 +0000

    Deploying to gh-pages from @ jsbay/vuepress@b2af8102f0459c608638c9f3e04e1c6a5cedbba7 🚀

---
+++ assets/multiple-remote.html.3ae44567.js
@@ -0,0 +1,13 @@
+import{_ as n}from"./_plugin-vue_export-helper.cdc0426e.js";import{o as s,c as e,e as a}from"./app.debf6241.js";const i={},t=a(`<h1 id="\u6DFB\u52A0\u591A\u4E2A-remote" tabindex="-1"><a class="header-anchor" href="#\u6DFB\u52A0\u591A\u4E2A-remote" aria-hidden="true">#</a> \u6DFB\u52A0\u591A\u4E2A remote</h1><div class="language-bash ext-sh line-numbers-mode"><pre class="language-bash"><code><span class="token comment"># \u521D\u59CB\u5316</span>
+<span class="token function">git</span> init
+<span class="token comment"># \u6DFB\u52A0\u8FDC\u7A0B\u4ED3\u5E93</span>
+<span class="token function">git</span> remote <span class="token function">add</span> origin git@gitxxxx.com
+<span class="token comment"># \u6DFB\u52A0\u7B2C\u4E8C\u4E2A\u4ED3\u5E93</span>
+<span class="token function">git</span> remote set-url <span class="token parameter variable">--add</span> origin ssh@gitxx2.com
+<span class="token comment"># \u6DFB\u52A0\u6240\u6709\u6587\u4EF6\u81F3\u672C\u5730\u6682\u5B58</span>
+<span class="token function">git</span> <span class="token function">add</span> <span class="token builtin class-name">.</span>
+<span class="token comment"># \u6DFB\u52A0\u63D0\u4EA4\u4FE1\u606F\u5E76\u63D0\u4EA4\u81F3\u672C\u5730\u4ED3\u5E93</span>
+<span class="token function">git</span> commit <span class="token parameter variable">-m</span> <span class="token string">&#39;init&#39;</span>
+<span class="token comment"># \u63A8\u9001\u81F3 \u8FDC\u7A0B\u4ED3\u5E93</span>
+<span class="token function">git</span> push <span class="token parameter variable">-u</span> origin master
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,2),c=[t];function l(o,r){return s(),e("div",null,c)}const p=n(i,[["render",l],["__file","multiple-remote.html.vue"]]);export{p as default};
